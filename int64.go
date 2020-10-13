4004257b2d4c4b54cc1c02963fe152f8be50d7c7
Faizan Khalid <IamFaizanKhalid@gmail.com>
2020-10-13 12:15:44 +0500

    Added NullInt64

---
+++ int64.go
@@ -0,0 +1,45 @@
+package nulltypes
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+)
+
+//NullInt64 is a wrapper around int64
+type NullInt64 struct {
+	Int64 int64
+	Valid bool
+}
+
+// Int64 method to get NullInt64 object from int64
+func Int64(Int64 int64) NullInt64 {
+	return NullInt64{Int64, true}
+}
+
+//MarshalJSON method is called by json.Marshal,
+//whenever it is of type NullInt64
+func (x *NullInt64) MarshalJSON() ([]byte, error) {
+	if !x.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(x.Int64)
+}
+
+// satisfy the sql.scanner interface
+func (x *NullInt64) Scan(value interface{}) error {
+	rt, ok := value.(int64)
+	if ok {
+		*x = NullInt64{rt, true}
+	} else {
+		*x = NullInt64{rt, false}
+	}
+	return nil
+}
+
+// satifies the driver.Value interface
+func (x NullInt64) Value() (driver.Value, error) {
+	if x.Valid {
+		return x.Int64, nil
+	}
+	return nil, nil
+}
